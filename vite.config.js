65467386928e9eec92fbcf0d1ab64c0fd3dd0cb5
jagklair7 <jagklair7@gmail.com>
2026-04-25 12:04:15 -0600

    remove pwa plugin for compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Invoice PWA',
-        short_name: 'Invoices',
-        theme_color: '#111',
-        background_color: '#111',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    
   ]
 })
