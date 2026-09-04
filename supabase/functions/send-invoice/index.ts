cea329210b13aae6df0127311f817a4c4d520364
jagklair7 <jagklair7@gmail.com>
2026-09-04 15:45:25 -0600

    modified /functions/send-invoice/index.ts

--- supabase/functions/send-invoice/index.ts
+++ supabase/functions/send-invoice/index.ts
@@ -1,7 +1,7 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 
 const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')!
-const FROM_EMAIL = Deno.env.get('FROM_EMAIL') || 'invoices@digital1now.com'
+const FROM_EMAIL = Deno.env.get('FROM_EMAIL') || 'do-not-reply@klair.ca'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
